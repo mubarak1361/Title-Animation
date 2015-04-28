4566bb0d5596ebe89bc077e8e14b86c982d8de71
mubarak1361 <disfrutarz@gmail.com>
2015-04-28 12:20:17 +0530

    Complex Loop

--- src/com/example/titleanimation/GolfFragment.java
+++ src/com/example/titleanimation/GolfFragment.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 public class GolfFragment extends Fragment{
 	
 	private View mView;
+	private int totalChapter = 6;
+	private int[] numberofTopics = new int[] {3,6,4,6,2,2,0};
+	private int subtopics = 1;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,6 +21,19 @@ public class GolfFragment extends Fragment{
 			super.onCreateView(inflater, container, savedInstanceState);
 			mView = inflater.inflate(R.layout.animation_viewpager_item, container, false);
 			((TextView)mView.findViewById(R.id.txt_viewpager_item_header)).setText("Golf Content");
+			
+			for (int i = 0; i <= totalChapter; i++) {
+				for (int j = 0; j <=numberofTopics[i]; j++) {
+					if((i!=1 && j!=3)||(i!=4 && j!=1)){
+						for (int k = 0; k <=subtopics; k++) {
+							
+						}
+					}
+					else{
+						
+					}
+				}
+			}
 			return mView;
 	}	
 	
